d00061a443a8c451790d728a16cfb141168cb42b
Fernando Herrera <fernando.herrera85@gmail.com>
2022-03-19 11:10:16 -0400

    Proyecto inicial

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+
