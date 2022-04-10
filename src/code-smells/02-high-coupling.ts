cdd9845dde57d2f463a116f8cbb1519755fa3d76
Fernando Herrera <fernando.herrera85@gmail.com>
2022-04-10 12:36:58 -0400

    Fin sección 4

---
+++ src/code-smells/02-high-coupling.ts
@@ -0,0 +1,74 @@
+(()=>{
+    // No aplicando el principio de responsabilidad única
+    type Gender = 'M'|'F';
+
+    // Alto Acoplamiento
+
+    class Person {
+        constructor(
+            public lastName: string,
+            public firstName: string,
+            public gender: Gender,
+            public birthdate: Date,
+        ){}
+    }
+
+    class User extends Person {
+        constructor(
+            public email: string,
+            public role: string,
+            private lastAccess: Date,
+            firstName: string,
+            lastName: string,
+            gender: Gender,
+            birthdate: Date,
+        ){
+            super(firstName, lastName, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+class UserSettings extends User {
+    constructor(
+        public workingDirectory: string,
+        public lastFolderOpen: string,
+        email     : string,
+        role      : string,
+        firstName : string,
+        lastName   : string,
+        gender    : Gender,
+        birthdate : Date,
+    ){
+        super(
+            email,
+            role,
+            new Date(),
+            firstName,
+            lastName,
+            gender,
+            birthdate
+        );
+    }
+}
+    
+
+
+    const userSettings = new UserSettings(
+        '/urs/home',
+        '/development',
+        'fernando@google.com',
+        'F',
+        'Fernando',
+        'Herrera',
+        'M',
+        new Date('1985-10-21')
+    );
+
+    console.log({ userSettings, credentials: userSettings.checkCredentials() });
+    
+})()
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './clean-code/07-tarea';
+// import './clean-code/07-tarea';
+import './code-smells/02-low-coupling';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
