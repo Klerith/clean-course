6d8aeba578bcd78d333c9fc9f288b5168792d920
Juan Felipe Arenas <juanfa107@gmail.com>
2022-12-04 16:06:21 -0500

    refactor clean code

---
+++ src/clean-code/04-homework.ts
@@ -0,0 +1,56 @@
+(() => {
+
+
+    // Resolver sin la triple condicional dentro del if
+    // includes? arrays?
+    function isRedFruit( fruit: string ): boolean {
+        return ['manzana', 'cereza', 'ciruela'].includes(fruit)
+    }
+
+
+    function getFruitsByColor( color: string ): string[] {
+
+        const fruitsByColor: {[key:string]: string[]} = {
+            'red': ['manzana','fresa'],
+            'yellow': ['piña','banana'],
+            'purple': ['moras','uvas']
+        }
+
+        
+        if (fruitsByColor[color]){
+            return fruitsByColor[color]
+        }
+        throw Error('the color must be: red, yellow, purple');
+    }
+
+    // Simplificar esta función
+    let isFirstStepWorking  = true;
+    let isSecondStepWorking = true;
+    let isThirdStepWorking  = true;
+    let isFourthStepWorking = true;
+
+    function workingSteps() {
+        if( !isFirstStepWorking ) return 'First step broken.';
+        if( !isSecondStepWorking ) return 'Second step broken.';
+        if( !isThirdStepWorking ) return 'Third step broken.';
+        if( !isFourthStepWorking ) return 'Fourth step broken.';
+        return 'Working properly!';
+
+    }
+
+
+    // isRedFruit
+    console.log({ isRedFruit: isRedFruit('cereza'), fruit: 'cereza' }); // true
+    console.log({ isRedFruit: isRedFruit('piña'), fruit: 'piña' }); // true
+
+    //getFruitsByColor
+    console.log({ redFruits: getFruitsByColor('red') }); // ['manzana', 'fresa']
+    console.log({ yellowFruits: getFruitsByColor('yellow') }); // ['piña', 'banana']
+    console.log({ purpleFruits: getFruitsByColor('purple') }); // ['moras', 'uvas']
+    // console.log({ pinkFruits: getFruitsByColor('pink') }); // Error: the color must be: red, yellow, purple
+
+    // workingSteps
+    console.log({ workingSteps: workingSteps() }); // Cambiar los valores de la línea 31 y esperar los resultados
+
+
+})();
