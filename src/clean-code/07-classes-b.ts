86b8ff35a099a12afa9eddb7927616b003b0dc77
Juan Felipe Arenas <juanfa107@gmail.com>
2023-01-05 22:06:48 -0500

    clean code -single responsibility concept

--- src/clean-code/07-classes-b.ts
+++ src/clean-code/07-classes-b.ts
@@ -1,106 +1,51 @@
-(() => {
-    
-    type Gender = 'M' | 'F';
+(()=>{
 
-    interface PersonProps {
-        name: string, 
-        gender: Gender, 
-        birthdate: Date
-    }
-    // without single responsibility
-    class Person{
-        birthdate: Date;
-        gender: string;
-        name: string;
+    //* Aplicar el principio de responsabilidad única
+    //* Priorizar la composición frente a la herencia
 
-        constructor({ name, gender, birthdate}: PersonProps){
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
-
+    type HtmlType = 'input'|'select'|'textarea'|'radio';
+   
+    class HtmlElement {
+        constructor(
+            public id: string,
+            public type: HtmlType,
+        ) {}
     }
 
-    interface userProps{
-        email:string;
-        role: string;
-        name: string;
-        gender: Gender;
-        birthdate: Date;
+    class InputAttributes {
+        constructor(
+            public value: string,
+            public placeholder: string,
+        ) {}
     }
 
+    class InputEvents  {
+        constructor() {}
 
-    class User extends Person {
-        
-        public lastAccess: Date;
-        public email: string;
-        public role: string;
-
-
-        constructor({
-            email,
-            role,
-            name, 
-            gender, 
-            birthdate
-        }: userProps){
-            super({name, gender, birthdate})
-            this.lastAccess = new Date();
-            this.email = email;
-            this.role = role;
-        }
-
-        checkCredenciales(){
-            return true;
-        }
-
-
-        
+        setFocus() {};
+        getValue() {};
+        isActive() {};
+        removeValue() {};
     }
 
 
-    interface UserSettingsProps{
-        workingDirectory: string,
-        lastOpenedFolder: string,
-        email: string,
-        role: string,
-        name: string, 
-        gender: Gender, 
-        birthdate: Date
-    }
+    //? Idea para la nueva clase InputElement
+    class InputElement{
+        public html: HtmlElement;
+        public attributes: InputAttributes;
+        public events: InputEvents;
 
-    class UserSettings extends User{
-
-        public workingDirectory: string;
-        public lastOpenedFolder: string;
-
-
-        constructor({
-            workingDirectory,
-            lastOpenedFolder,
-            email,
-            role,
-            name, 
-            gender, 
-            birthdate
-        }: UserSettingsProps){
-            super({email, role, name, gender, birthdate})
-            this.workingDirectory = workingDirectory;
-            this.lastOpenedFolder = lastOpenedFolder
+        constructor(value: string, placeholder: string, id: string){
+            this.html = new HtmlElement(id, 'input');
+            this.attributes = new InputAttributes(value, placeholder)
+            this.events = new InputEvents();
         }
     }
 
+    const nameField = new InputElement('Felipe', 'Enter first name', 'txtName');
 
-    const x = new UserSettings({
+    console.log({ nameField });
 
-        workingDirectory:  'usr/home',
-        lastOpenedFolder: '/home',
-        email: 'juanfa1207@gmail.com',
-        role:  'Admin',
-        name:  'Juan Arenas',
-        gender: 'M',
-        birthdate: new Date('1996-05-30'),
-    })
 
-        console.log({x})
-})();
+
+})()
