6a01d1630291234cb597a67605a8d75974481c77
alberto8812 <cvelascosaavedra@gmail.com>
2023-09-09 22:21:10 -0500

    name num,function,booleanos

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,52 @@
+(() => {
+
+    // Ejemplo
+    // Archivos a evaluar - files to evaluate
+    const filesToEvaluate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const filesToDelete = filesToEvaluate.map( data => data.flagged );
+
+    
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+    // día de hoy - today
+    const today = new Date();
+    
+    // días transcurridos - elapsed time in days
+    const elapsedTimeIndays: number = 23;
+    
+    // número de archivos en un directorio - number of files in directory
+    const NumberOfFilesInDirectory = 33;
+    
+    // primer nombre - first name
+    const firsName = 'Fernando';
+    
+    // primer apellido - last name
+    const LastName = 'Herrera';
+
+    // días desde la última modificación - days since modification
+    const dayLastModification = 12;
+    
+    // cantidad máxima de clases por estudiante - max classes per student
+    const maxClassesPerStudent = 6;
+
+
+})();
