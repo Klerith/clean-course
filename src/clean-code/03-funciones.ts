69903be1458b0effe30382949de2a7fa20073091
alicia-granados <aliciagranados35@gmail.com>
2024-01-25 15:34:07 -0600

    feat: :sparkles: ejercicio con funciones

---
+++ src/clean-code/03-funciones.ts
@@ -0,0 +1,53 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getBioActorById( id: string ) {
+        console.log({ id });
+    }
+    
+
+    interface Movie{
+        cast: string[];
+        description: string;
+        rating: number;
+        title: string;
+    }
+    // Crear una película
+    function createMovie({title, description, rating, cast}: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+
+    interface Actor{
+        birthdate: Date;
+        fullName: string;
+    }
+
+    function checkFullName(fullName: string){
+        console.log({fullName})
+        return true 
+    }
+    // Crea un nuevo actor
+    function createActor( {fullName, birthdate}: Actor): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if (checkFullName(fullName)) return false;
+
+        console.log('Crear actor' , birthdate);
+        return true;        
+
+    }
+
+})();
