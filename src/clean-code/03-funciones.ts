c8aac32e25e994862ab694634e9b39af4b7b384b
alicia-granados <aliciagranados35@gmail.com>
2024-01-25 15:47:30 -0600

    feat: :sparkles: detalles adicionales sobre las funciones

--- src/clean-code/03-funciones.ts
+++ src/clean-code/03-funciones.ts
@@ -50,4 +50,14 @@
 
     }
 
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) : number => {
+
+        if ( isDead )  return 1500;
+        
+        if ( isSeparated ) return 2500;
+        
+        return (isRetired)  ? 3000 : 4000;
+    
+    }
+
 })();
