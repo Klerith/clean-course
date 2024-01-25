30fc1e6df8f8862adb9c51009f6a7b23a5dfdf60
alicia-granados <aliciagranados35@gmail.com>
2024-01-25 14:19:12 -0600

    feat: :sparkles: Nombres pronunciales y expresivos

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,52 @@
+(() => {
+
+    // Ejemplo
+    // Archivos a evaluar - files to evaluate
+    const filesToEvaluate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const filesToDelete = filesToEvaluate.map( file => file.flagged );
+
+    // Malos nombres
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+    // día de hoy - today
+    const today = new Date();
+    
+    // días transcurridos - elapsed time in days
+    const elapsedTimeInDays: number = 23;
+    
+    // número de archivos en un directorio - number of files in directory
+    const numberFilesInDirectory = 33;
+    
+    // primer nombre - first name
+    const firstName = 'Fernando';
+    
+    // primer apellido - last name
+    const lastName = 'Herrera';
+
+    // días desde la última modificación - days since modification
+    const daysSinceLastModificaction = 12;
+    
+    // cantidad máxima de clases por estudiante - max classes per student
+    const maxClassesPerStudent = 6;
+
+
+})();
