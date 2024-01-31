5f348cd298ad8a1b20ade289282a9e1c0ccf9c85
alicia-granados <aliciagranados35@gmail.com>
2024-01-31 15:47:53 -0600

    feat: :sparkles: Aplicando DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,11 +7,30 @@ class Product {
         public size: string = "",
     ){}
 
+    isProductReady(): boolean{
+        for ( const key in this ){
+            switch(typeof this[key]){
+                case 'string':
+                    if( (<string><unknown>this[key]).length <= 0 ) throw Error(`${key} is empty`);
+                break;
+                case 'number':
+                    if( (<number><unknown>this[key]) <= 0 ) throw Error(`${key} is zero`);
+                break;
+                default:
+                    throw Error(`${typeof this[key]} is not supported`);
+            }
+        }
+        return true
+    }
+
     toString(){
         // No dry
-        if( this.name.length <= 0) throw Error('name is empty')
+        /*if( this.name.length <= 0) throw Error('name is empty')
         if( this.price <= 0) throw Error('price is zero')
         if( this.size.length <= 0) throw Error('size is empty')
+        */
+
+       if (!this.isProductReady) return;
 
         return  ` ${this.name}, (${this.price}), ${this.size}`
     }
