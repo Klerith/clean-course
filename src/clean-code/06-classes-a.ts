a254245f30d25e6627f84195aae300406af9fb1b
alicia-granados <aliciagranados35@gmail.com>
2024-02-19 16:26:17 -0600

    feat: :sparkles: Herencia Problematica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+    // NO aplicando el principio de resposnsabilidad unica
+
     type Gender = 'M'|'F';
     class Person {
         /* FORMA LARGA
@@ -22,7 +24,49 @@
         ){}
     }
 
+    class User extends Person{
+        public  lasAccess:Date
+        constructor(
+            public email: string,
+            public role: string,
+            
+            name: string,
+            gender: Gender,
+            birthdate:Date,
+        ){
+            super(name,gender,birthdate);
+            this.lasAccess = new Date();
+        }
 
-    const newPerson = new Person('Fernando', 'M', new Date('1985-10-21'));
-    console.log({newPerson})
+        checkCredentials(){
+            return true;
+        }
+    }
+
+    class UserSettings extends User{
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate:Date,
+        ){ 
+            super(email, role, name, gender, birthdate);
+        }
+    }
+    
+
+    const userSettings = new UserSettings(
+        "/usr/home",
+        "/home",
+        "fernando@google.com",
+        "Admin",
+        "Fernando",
+        "M",
+        new Date("1985-10-21")
+    )
+    
+    console.log({userSettings, areCredentialsValid: userSettings.checkCredentials()})
 })();
